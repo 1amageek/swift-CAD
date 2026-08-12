b7b88aa4f6324a1e81608994ebb4468f887bf52b
1amageek <tmy0x3@icloud.com>
2026-08-12 22:05:43 +0900

    Restore five-minute limit on cone-torus Boolean tests
    With diagnostics removed the release-lane suite runs at 38-117 seconds
    per test, well inside the standard limit.

--- Tests/SwiftCADTests/PrimitiveGeneralConeTorusBooleanIntegrationTests.swift
+++ Tests/SwiftCADTests/PrimitiveGeneralConeTorusBooleanIntegrationTests.swift
@@ -7,7 +7,7 @@ import SwiftCAD
 
 @Suite("Primitive general cone-torus Boolean integration", .serialized)
 struct PrimitiveGeneralConeTorusBooleanIntegrationTests {
-    @Test(.timeLimit(.minutes(10)))
+    @Test(.timeLimit(.minutes(5)))
     func intersectionProducesValidatedExactSolid() throws {
         try assertExactResult(
             evaluate(operation: .intersect),
@@ -15,7 +15,7 @@ struct PrimitiveGeneralConeTorusBooleanIntegrationTests {
         )
     }
 
-    @Test(.timeLimit(.minutes(10)))
+    @Test(.timeLimit(.minutes(5)))
     func differenceProducesValidatedExactSolid() throws {
         try assertExactResult(
             evaluate(operation: .difference),
@@ -23,7 +23,7 @@ struct PrimitiveGeneralConeTorusBooleanIntegrationTests {
         )
     }
 
-    @Test(.timeLimit(.minutes(10)))
+    @Test(.timeLimit(.minutes(5)))
     func unionProducesValidatedExactSolid() throws {
         try assertExactResult(
             evaluate(operation: .union),
